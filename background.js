33878c7c164c2c6551e12ffdbf9bc8663512f471
nukobito <nukobito@live.jp>
2013-07-16 22:22:59 +0900

    色々模索中。

--- background.js
+++ background.js
@@ -1,1 +1,29 @@
-chrome.browserAction.setBadgeText( { 'text': '240' } );
+/*
+var counter = 0;
+
+function update() {
+    chrome.browserAction.setBadgeText( { 'text': String( counter ) } );
+    chrome.alarms.create( 'MoEGarden', { delayInMinutes: 5 / 60 } );
+}
+
+chrome.runtime.onInstalled.addListener( function () {
+    update();
+} );
+
+chrome.alarms.onAlarm.addListener( function ( alarm ) {
+    if ( !alarm )
+        return;
+    
+    switch ( alarm.name ) {
+        case 'MoEGarden':
+            counter++;
+            if ( 9999 < counter ) counter = 0;
+            update();
+            break;
+    }
+} );
+*/
+
+chrome.runtime.onStarted.addListener( function () {
+    console.log( 'onStarted' );
+} );
