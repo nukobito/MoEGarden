f371ea24dbc715dbb915cb95010ca50a6b90e42a
nukobito <nukobito@live.jp>
2013-07-26 23:30:15 +0900

    定期保存。

---
+++ scripts/background.js
@@ -0,0 +1,39 @@
+(function () {
+    function onInstalled() {
+        console.log( 'onInstalled' );
+    }
+    
+    function onAlarm( alarm ) {
+        console.log( 'onAlarm' );
+    }
+    
+    chrome.runtime.onInstalled.addListener( onInstalled );
+    chrome.alarms.onAlarm.addListener( onAlarm );
+}( window ));
+
+
+
+
+//var ALARM_NAME = 'MoEGarden';
+//var counter = 0;
+//
+//chrome.browserAction.setBadgeText( { 'text': String( counter ) } );
+//
+//chrome.alarms.onAlarm.addListener( function ( alarm ) {
+//    switch ( alarm.name ) {
+//        case ALARM_NAME:
+//            counter++;
+//            if ( 9999 < counter ) counter = 0;
+//            chrome.browserAction.setBadgeText( { 'text': String( counter ) } );
+//            break;
+//    }
+//} );
+//
+//function startAlarm() {
+//    chrome.alarms.clear( ALARM_NAME );
+//    chrome.alarms.create( ALARM_NAME, { when: 0, periodInMinutes: 1 / 60 } );
+//}
+//
+//function stopAlarm() {
+//    chrome.alarms.clear( ALARM_NAME );
+//}
